1e17eb88bc179ef4df0dbf8888287ab82c2bee9d
sanober riaz <sanober@sanober.me>
2018-01-23 21:47:18 +0000

    automatically backed up by learn

--- if.js
+++ if.js
@@ -1,5 +1,9 @@
-var pokemon = "Pikachu"
 
-if (pokemon == "Pikachu") {
-  console.log("I choose you Pikachu!!");
+function greaterThanTen(n){
+  if (n>=10) {
+    return "The number is greater than or equal to ten.";
+  }else {
+    return "number is less than 10";
+  }
 }
+
