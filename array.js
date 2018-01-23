86117a4f40a1479cc4a7250955b03c03a86d0577
sanober riaz <sanober@sanober.me>
2018-01-23 21:47:22 +0000

    Done.

--- array.js
+++ array.js
@@ -1,34 +1,43 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
+ return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
-
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index) {
+  return array[index];
 
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+  array.shift();
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+  array.pop();
+  return array;
 }
 
 function removeElementFromEndOfArray(array) {
-
+  let l = array.length;
+  return array.slice(0,l-1);
 }
